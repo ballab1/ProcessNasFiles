ccf167fbcc1fd15d6a031b3d28b548e32cdb3dde
Ballantyne, Bob <ballantyne.robert@gmail.com>
2021-06-04 19:56:22 -0400

    execute on postgres container

--- data.sh
+++ data.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+#!/bin/bash -x
 
 function etl()
 {
     [ "${IS_VERBOSE:-}" ] && VERBOSE=( '--no-align' '--tuples-only' '--quiet' '--echo-queries' '--echo-hidden' )
 
-    "$PSQL" --host="$DBMS_host" \
-            --port="$DBMS_port" \
-            --dbname="$DBMS_dbname" \
-            --username="$DBMS_username" \
-            --variable=ON_ERROR_STOP=1 \
-            "${VERBOSE[@]}" "$@"
+    $PSQL --host="$DBMS_host" \
+          --port="$DBMS_port" \
+          --dbname="$DBMS_dbname" \
+          --username="$DBMS_username" \
+          --variable=ON_ERROR_STOP=1 \
+          ${VERBOSE[@]} $@
 
 }
 
@@ -17,9 +17,10 @@ DBMS_host=10.3.1.16
 DBMS_port=5432
 DBMS_dbname=bobb
 DBMS_username=bobb
-PSQL=/usr/local/bin/psql
+PSQL='docker exec postgresql /usr/local/bin/psql'
+IS_VERBOSE='true'
 
-etl -f ./setupDatabase.sql
-etl -f ./processFileData.sql "--variable=jsonfile=$1"
-etl -f ./buildTranslation.sql --output=data.txt
-etl "--command=copy (select row_to_json(nasdata) from nasinfo.nasdata) to stdout;" --output=data.json
+etl -f "${WORKSPACE}/setupDatabase.sql"
+etl -f "${WORKSPACE}/processFileData.sql" "--variable=jsonfile=$1"
+etl -f "${WORKSPACE}/buildTranslation.sql" "--output=${WORKSPACE}/data.txt"
+etl "--command=copy (select row_to_json(nasdata) from nasinfo.nasdata) to stdout;" "--output=${WORKSPACE}/data.json"
